ba43408dcf68366689f7c28256a0f171634f3b6c
Christian Oaksford <39574029+coaksford@users.noreply.github.com>
2023-12-20 23:09:12 -0600

    Add renaming keymap.

--- lua/zuli/neovide.lua
+++ lua/zuli/neovide.lua
@@ -5,6 +5,6 @@ if vim.g.neovide then
     vim.g.neovide_floating_blur_amount_x = 9.0
     vim.g.neovide_floating_blur_amount_y = 9.0
 
-    vim.g.neovide_transparency = 0.9
+    vim.g.neovide_transparency = 1.0
 end
 
