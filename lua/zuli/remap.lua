add46291185ee0cbd0c521ddcf0f8e0fdfd41541
coaksford <39574029+coaksford@users.noreply.github.com>
2024-04-25 20:55:18 -0500

    visual tweaks to search

--- lua/zuli/remap.lua
+++ lua/zuli/remap.lua
@@ -31,10 +31,9 @@ vim.keymap.set( 'n', '<leader>sp', tsproj.projects, { desc = "[S]earch [P]roject
 vim.keymap.set( 'n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set( 'n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
 vim.keymap.set( 'n', '<leader>f', function()
-  -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-    winblend = 10,
-    previewer = false,
+    winblend = 30, -- 70% opacity
+    previewer = true, -- show the context of the highlighted option
   })
 end, { desc = '[f] Fuzzily search in current buffer' })
 
