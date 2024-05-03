adffb7fc64e7274d646e0c8854a563d6d102d633
coaksford <39574029+coaksford@users.noreply.github.com>
2024-05-03 14:21:46 -0500

    Adjust scale with leader+v or +V in neovide

--- lua/zuli/neovide.lua
+++ lua/zuli/neovide.lua
@@ -5,8 +5,8 @@ if vim.g.neovide then
     vim.g.neovide_floating_blur_amount_x = 9.0
     vim.g.neovide_floating_blur_amount_y = 9.0
 
-    vim.keymap.set( 'n', '<leader>v+', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end, { desc = 'Increase Neovide scale' })
-    vim.keymap.set( 'n', '<leader>v-', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end, { desc = 'Decrease Neovide scale' })
+    vim.keymap.set( 'n', '<leader>V', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1 end, { desc = 'Increase Neovide scale' })
+    vim.keymap.set( 'n', '<leader>v', function() vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1 end, { desc = 'Decrease Neovide scale' })
     
     vim.g.neovide_transparency = 1.0
 end
