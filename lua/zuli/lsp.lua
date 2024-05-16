08de74020f4b29672dea167ad3ef598cf3df95bd
coaksford <39574029+coaksford@users.noreply.github.com>
2024-05-15 23:35:07 -0500

    Clean up diagnostics that vim is undefined when editing nvim config.

--- lua/zuli/lsp.lua
+++ lua/zuli/lsp.lua
@@ -45,6 +45,9 @@ local servers = {
     Lua = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
+      diagnostics = {
+        globals = { "vim" }
+      },
     },
   },
 }
