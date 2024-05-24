8ae7cded806bddfd47915dedc56bcaf984c7ebf2
coaksford <39574029+coaksford@users.noreply.github.com>
2024-05-24 10:53:31 -0500

    Change gutter symbols.

--- lua/zuli/lsp.lua
+++ lua/zuli/lsp.lua
@@ -98,11 +98,11 @@ local sign = function(opts)
   })
 end
 
-sign({name = 'DiagnosticSignError', text = ''})
-sign({name = 'DiagnosticSignWarn', text = ''})
-sign({name = 'DiagnosticSignHint', text = ''})
-sign({name = 'DiagnosticSignInfo', text = ''})
-
+sign({name = 'DiagnosticSignError', text = ''})
+sign({name = 'DiagnosticSignWarn', text = ''})
+sign({name = 'DiagnosticSignHint', text = '?'})
+sign({name = 'DiagnosticSignInfo', text = 'i'})
+-- 
 vim.diagnostic.config({
     virtual_text = false,
     signs = true,
