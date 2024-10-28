4c5708b7f395147288176d6c3ecc3cd67c097a99
Christian Oaksford <39574029+coaksford@users.noreply.github.com>
2024-10-28 13:44:50 -0500

    Wrap search lines instead of hiding overflow.

--- lua/zuli/search.lua
+++ lua/zuli/search.lua
@@ -3,6 +3,7 @@
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    wrap_line = true,
     mappings = {
       i = {
         ['<C-u>'] = false,
