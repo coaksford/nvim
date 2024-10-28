dbf6f61c82a068b190f7c144c6438d90aa586eb4
Christian Oaksford <39574029+coaksford@users.noreply.github.com>
2024-10-28 18:41:20 -0500

    Update packages to newer versions, fix some which-key warnings about deprecated usage patterns.

--- lua/zuli/lsp.lua
+++ lua/zuli/lsp.lua
@@ -19,15 +19,16 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+local wk = require('which-key')
+wk.add({
+  {'<leader>c', group = '[C]ode' },
+  {'<leader>d', group = '[D]ocument' },
+  {'<leader>g', group = '[G]it' },
+  {'<leader>h', group = 'More git' },
+  {'<leader>r', group = '[R]ename' },
+  {'<leader>s', group = '[S]earch' },
+  {'<leader>w', group = '[W]orkspace' },
+})
 
 -- LSP
 require('mason').setup()
