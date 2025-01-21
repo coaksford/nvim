b44322853ea2e58d2ea2db09ce13a825db63eb65
Christian Oaksford <39574029+coaksford@users.noreply.github.com>
2025-01-20 20:47:01 -0600

    stop rust-analyzer from being really annoying by disregarding its complaints

--- lua/zuli/lsp.lua
+++ lua/zuli/lsp.lua
@@ -34,6 +34,17 @@ wk.add({
 require('mason').setup()
 require('mason-lspconfig').setup()
 
+-- Ignore rust analyzer complaints
+for _, method in ipairs({ 'textDocument/diagnostic', 'workspace/diagnostic' }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
+
 local servers = {
   clangd = {},
   gopls = {},
