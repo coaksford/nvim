e57180dce400352c97fd97c22ddb52a88605dcd5
Zulikath <69781787+zulikath@users.noreply.github.com>
2025-03-31 22:39:25 -0500

    enable blink.cmp keymap fallbacks

--- lua/plugins/completion/init.lua
+++ lua/plugins/completion/init.lua
@@ -14,25 +14,14 @@ return {
   ---@module 'blink.cmp'
   ---@type blink.cmp.Config
   opts = {
-    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-    -- 'super-tab' for mappings similar to vscode (tab to accept)
-    -- 'enter' for enter to accept
-    -- 'none' for no mappings
-    --
-    -- All presets have the following mappings:
-    -- C-space: Open menu or open docs if already open
-    -- C-n/C-p or Up/Down: Select next/previous item
-    -- C-e: Hide menu
-    -- C-k: Toggle signature help (if signature.enabled = true)
-    --
     -- See :h blink-cmp-config-keymap for defining your own keymap
     keymap = { 
-      preset = 'none',
+      preset = 'none', -- unmap everything first
       ['<Tab>'] = {'accept', 'fallback'},
-      ['<Down>'] = {'select_next'},
-      ['<Up>'] = {'select_prev'},
-      ['<Right>'] = {'show_documentation'},
-      ['<Left>'] = {'hide_documentation'},
+      ['<Down>'] = {'select_next', 'fallback'},
+      ['<Up>'] = {'select_prev', 'fallback'},
+      ['<Right>'] = {'show_documentation', 'fallback'},
+      ['<Left>'] = {'hide_documentation', 'fallback'},
     },
 
     appearance = {
